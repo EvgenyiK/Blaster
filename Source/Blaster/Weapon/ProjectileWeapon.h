777941b28047e900189dcb320711bccec91bf9b8
ONeil <evgenprogrammer@gmail.com>
2024-04-20 16:03:28 +0300

    projectile weapon class

---
+++ Source/Blaster/Weapon/ProjectileWeapon.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "ProjectileWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AProjectileWeapon : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
